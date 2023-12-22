6840bd5aba0990d0ca87d42119517398d28eb61a
emmapreysing <emma@preysingmail.com>
2023-12-22 15:54:26 +0100

    file

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Owner
+  has_many :kitchens
+  has_many :bookings_as_owner, through: :kitchens, source: :bookings
+  # User
+  has_many :bookings
+  has_one_attached :photo
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+Rails.application.routes.draw do
+  root "kitchens#index"
+
+  resources :kitchens do
+    resources :bookings, only: %i[new create show]
+  end
+
+  # resources :bookings, only: %i[destroy new create]
+
+  resources :bookings do
+    resources :reviews, only: [:new, :create]
+  end
+
+  get "dashboard", to: "pages#dashboard"
+
+  devise_for :users
+
+  # root to: "pages#home"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+  resources :kitchens
+end
